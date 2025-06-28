c75376c53fb4dcd5281443cf01bcddb1cf8ab57b
INZAMAMULHAQUE-0 <ihaque3040@gmail.com>
2025-06-28 15:04:39 +0530

    for review

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -40,7 +40,7 @@ const LandingPage = () => {
         </div>
 
         <button
-          onClick={() => navigate('/login')}
+          onClick={() => navigate('/order')}
           className="px-6 py-3 bg-[#8b4513] text-white rounded-lg shadow hover:bg-[#7a3e00] transition"
         >
           Select Product &gt;
